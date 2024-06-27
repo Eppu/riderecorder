b78f9295c995b9ae473fe475ba1b2026b6163763
Eetu Eskelinen <eetuesk@gmail.com>
2024-06-27 15:58:52 +0300

    Navigation improvements

--- App.js
+++ App.js
@@ -3,40 +3,64 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createStackNavigator } from '@react-navigation/stack';
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Text } from 'react-native';
 import Homescreen from './screens/HomeScreen';
 import Aboutscreen from './screens/AboutScreen';
 import MapScreen from './screens/MapScreen';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import Icon from '@react-native-vector-icons/ionicons';
+import FontAwesome6 from 'react-native-vector-icons/FontAwesome6';
+import { StatusBar } from 'expo-status-bar';
 
 const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    // Native navigation container example
-    // <NavigationContainer>
-    //   <Stack.Navigator initialRouteName="Home">
-    //     <Stack.Screen name="Home" component={Homescreen} />
-    //     <Stack.Screen name="About" component={Aboutscreen} />
-    //   </Stack.Navigator>
-    // </NavigationContainer>
-    <NavigationContainer documentTitle={''}>
-      <Tab.Navigator
-        initialRouteName="Map"
-        tabBarOptions={{
-          activeTintColor: 'tomato',
-          inactiveTintColor: 'gray',
-        }}
-      >
-        <Tab.Screen
-          options={{ headerShown: false }}
-          name="Map"
-          component={MapScreen}
-        />
-        <Tab.Screen name="Home" component={Homescreen} />
-        <Tab.Screen name="About" component={Aboutscreen} />
-      </Tab.Navigator>
-    </NavigationContainer>
+    <SafeAreaProvider>
+      <NavigationContainer>
+        <Tab.Navigator
+          initialRouteName="Map"
+          screenOptions={({ route }) => ({
+            tabBarIcon: ({ focused, color, size }) => {
+              let iconName;
+              if (route.name === 'Home') {
+                iconName = 'home';
+                // iconName = 'house'; // FA6 alternative
+              } else if (route.name === 'About') {
+                iconName = 'person';
+                // iconName = 'user'; // FA6 alternative
+              }
+              return <Icon name={iconName} size={size} color={color} />;
+            },
+          })}
+          tabBarOptions={{
+            activeTintColor: 'tomato',
+            inactiveTintColor: 'gray',
+          }}
+        >
+          <Tab.Screen name="Home" component={Homescreen} />
+          <Tab.Screen
+            name="Ride"
+            component={MapScreen}
+            options={{
+              headerShown: false,
+              // override the tab bar icon since we need the motorcycle icon from FA6
+              tabBarIcon: ({ focused, color, size }) => (
+                <FontAwesome6
+                  name={'motorcycle'}
+                  size={24}
+                  color={color}
+                  solid
+                />
+              ),
+            }}
+          />
+          <Tab.Screen name="About" component={Aboutscreen} />
+        </Tab.Navigator>
+      </NavigationContainer>
+      <StatusBar style="auto" />
+    </SafeAreaProvider>
   );
 }
 
